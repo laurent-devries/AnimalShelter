7109b947b92e76273bae2fc95d28e9fe43cb6b3c
laurent-devries <laurent-devries@outlook.com>
2015-10-06 11:27:43 +0200

    AddUnitTestProject

---
+++ UnittestsAnimalshelter/UnittesttAS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalShelter;
+
+namespace UnittestsAnimalshelter
+{
+    [TestClass]
+    public class UnittesttAS
+    {
+        [TestMethod]
+        public void TestAddAnimal()
+        {
+                        
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            
+        }
+    }
+}
